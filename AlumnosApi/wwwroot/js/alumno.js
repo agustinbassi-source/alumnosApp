c25286c8e1a5e8ae4a30a001f61ce4d3b5b364cd
Agustin Bassi <a.bassi@winsysgroup.com>
2023-05-12 09:39:00 -0300

    Agregado de helper y common js

---
+++ AlumnosApi/wwwroot/js/alumno.js
@@ -0,0 +1,85 @@
+﻿
+const url = '/api/alumnos';
+const urlCursos = '/api/cursos';
+
+const obtenerCursos = async () => {
+
+  console.log("cargando cursos...");
+
+  let cursos = [];
+
+  cursos = await fetchGET(urlCursos);
+
+  console.log("cursos", cursos)
+
+  let selectCursos = document.getElementById("selectCurso");
+
+  cursos.forEach(curso => {
+
+    let option = document.createElement("option");
+
+    option.text = curso.nombre;
+    option.value = curso.id;
+
+    selectCursos.add(option);
+
+
+  })
+
+}
+
+obtenerCursos();
+
+const obtenerAlumnos = async () => {
+
+  console.log("cargando alumnos...");
+
+  let alumnos = [];
+
+  alumnos = await fetchGET(url)
+
+  document.getElementById("dvAlumnos").appendChild(toTable(alumnos))
+
+  console.log("alumnos", alumnos)
+
+  let selectAlumnos = document.getElementById("selectAlumno");
+
+  selectAlumnos.innerHTML = '';
+
+  alumnos.forEach(alumno => {
+
+    let option = document.createElement("option");
+
+    option.text = alumno.nombre + " " + alumno.apellido;
+    option.value = alumno.id;
+
+    selectAlumnos.add(option);
+
+  })
+
+}
+
+obtenerAlumnos();
+
+const crear = async () => {
+
+  let data = {};
+
+  data.nombre = document.getElementById("txtNombre").value;
+  data.cursoid = parseInt(document.getElementById("selectCurso").value);
+
+  console.log(data)
+
+  fetchPOST(url, data)
+
+};
+
+const eliminar = async () => {
+
+  let id = parseInt(document.getElementById("selectAlumno").value)
+
+  fetchDELETE(url + "/" + id);
+
+  obtenerAlumnos();
+
+}
---
+++ AlumnosApi/wwwroot/js/commonFetch.js
@@ -0,0 +1,36 @@
+﻿
+const fetchPOST = async (url, data) => {
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+}
+
+const fetchGET = async (url) => {
+
+  let dataResponse;
+
+  await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      dataResponse = data;
+    });
+
+  console.log('api response', dataResponse)
+
+  return dataResponse;
+
+}
+
+const fetchDELETE = async (url) => {
+
+  await fetch(url, {
+    method: 'DELETE'
+  })
+
+}
